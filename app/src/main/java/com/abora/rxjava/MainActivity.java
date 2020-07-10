52bed60ec306480b3e8383667e67dbce9909c071
Abora <https://github.com/abora97>
2020-07-10 18:53:23 +0200

    cold Observable

--- app/src/main/java/com/abora/rxjava/MainActivity.java
+++ app/src/main/java/com/abora/rxjava/MainActivity.java
@@ -1,13 +1,33 @@
 package com.abora.rxjava;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.rxjava3.core.Observable;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+      //  Observable<Long> cold;
+        Observable<Long> cold=Observable.intervalRange(0,5,0,1, TimeUnit.SECONDS);
+
+        cold.subscribe(i-> Log.d(TAG,"Observable cold output 1: "+i));
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        cold.subscribe(i-> Log.d(TAG,"Observable cold output 2: "+i));
     }
 }
